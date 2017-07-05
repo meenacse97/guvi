cd2e9b150d65e1e27a26750df769e4b12616b604
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-05 12:03:00 +0530

    Add files via upload

---
+++ multp.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class multp {
+	public static void main(String[] args) {
+		String a;
+		String b;
+		Scanner s=new Scanner(System.in);
+		a=s.next();
+		b=s.next();
+		int c=Integer.parseInt(a);
+		int d=Integer.parseInt(b);
+		int e=c*d;
+		String m=String.valueOf(e);
+		System.out.println(m);
+	}
+}
