c537699e304b6db918b2f8d9a852275fcf261e21
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-05 12:45:56 +0530

    Add files via upload

---
+++ strint.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class strint {
+	public static void main(String[] args){
+		String a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the string:");
+		a=s.nextLine();
+		int n=Integer.parseInt(a);
+		System.out.println(n);
+		
+		
+	}
+
+}
